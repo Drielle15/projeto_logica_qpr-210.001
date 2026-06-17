86d92823450101e2bc287ed504e15c722458f914
Drielle Dantas <drielle.dantas@aluno.senai.br>
2026-06-17 14:02:29 -0300

    Atualizações no index.html e js/script_repeticoes.js

--- js/script_repeticoes.js
+++ js/script_repeticoes.js
@@ -2,19 +2,20 @@
 const divFor = document.querySelector('#div-for')
 
 for(i = 0;i < 10; i++ ){
-    console.log(i, 'Amor')
-  divFor.innerHTML += `${i} - Amor <br>`
+    console.log(i, 'Dinheiro')
+   divFor.innerHTML += `${i} - Dinheiro <br> `
+
 }
 
 //CONTADOR/ACUMULADOR
 const inputNum =  document.querySelector('#num')
-const btnNum =  document.querySelector('#btn - num')
+const btnNum =  document.querySelector('#btn-num')
 const divContAcum =  document.querySelector('#div-cont-acum')
 
 let cont = 0
 let acum = 0.0
-btnNum.addEventListener('click', (evt)=>{
 
+    btnNum.addEventListener('click', (evt)=>{
     let numDigitado = Number(inputNum.value)
 
     cont++
@@ -25,3 +26,9 @@ btnNum.addEventListener('click', (evt)=>{
 
     inputNum.value = ''
 })
+
+//CONTROLANDO FOR
+const inputFrase = document.querySelector('#frase')
+const inputNumRepeticao = document.querySelector('#num-repeticao')
+const btnFrase = document.querySelector('#btn-frase')
+const divFrase = document.querySelector('#div-frase')
